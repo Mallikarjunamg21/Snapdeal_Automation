6cb1180f22f4d94954b8f4cad991b5ff76509825
Mallikarjunamg21 <mallikarjunamg21@gmail.com>
2023-07-26 03:15:55 +0530

    as eclipse

---
+++ src/main/java/com/snapdeal/base/ListenerTest.java
@@ -0,0 +1,10 @@
+package com.snapdeal.base;
+
+import org.testng.ITestContext ;
+import org.testng.ITestListener ;
+import org.testng.ITestResult ;
+
+class ListenerTest implements ITestListener
+{
+
+}
--- src/test/java/com/snapdeal/tests/HomePageTests.java
+++ src/test/java/com/snapdeal/tests/HomePageTests.java
@@ -11,17 +11,18 @@ import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
 import org.testng.Assert;
 import org.testng.ITest;
-import org.testng.annotations.AfterMethod;
-import org.testng.annotations.BeforeMethod;
-import org.testng.annotations.DataProvider;
-import org.testng.annotations.Test;
+import org.testng.annotations.*;
 
 import com.snapdeal.base.TestBase;
+import ru.yandex.qatools.ashot.AShot;
+import ru.yandex.qatools.ashot.Screenshot;
+import ru.yandex.qatools.ashot.shooting.ShootingStrategies;
 
 import javax.imageio.ImageIO;
 import java.awt.*;
 import java.awt.event.KeyEvent;
 import java.io.File;
+import java.io.IOException;
 import java.time.Duration;
 import java.util.ArrayList;
 import java.util.List;
@@ -99,48 +100,61 @@ public class HomePageTests extends TestBase
 			Actions mouse = new Actions(driver);
 			mouse.moveToElement(signIn);
 			mouse.moveToElement(register).click().build().perform();
-		//WebDriverWait wait = new WebDriverWait(driver,Duration.ofSeconds(10));
+		WebDriverWait wait = new WebDriverWait(driver,Duration.ofSeconds(10));
 //			wait.until(ExpectedConditions.visibilityOf(driver.findElement(By.xpath("//div[id='signin_box']"))));
 			//wait.until(ExpectedConditions.frameToBeAvailableAndSwitchToIt(By.id("iframeLogin")));
 			WebElement frame =driver.findElement(By.xpath("//div[@id='iframeContainer']/iframe"));
-
-			try {
-				driver.switchTo().frame("iframeLogin");
+			String frameUrl = frame.getAttribute("src");
+			driver.switchTo().frame("iframeLogin");
 				// Perform some action on the element (e.g., click, sendKeys, etc.)
-				String frameUrl = frame.getAttribute("src");
-				Robot k= new Robot();
-				k.keyPress(KeyEvent.VK_CONTROL);
-				k.keyPress(KeyEvent.VK_T);
-				k.keyRelease(KeyEvent.VK_CONTROL);
-				k.keyRelease(KeyEvent.VK_T);
-				driver.navigate().to(frameUrl);
 
-			} catch (StaleElementReferenceException e) {
-
-				System.out.println("Hi retry");
-			}
 			//Thread.sleep(5000);
+			wait.until(ExpectedConditions.presenceOfElementLocated(By.xpath("//input[@id='userName']")));
 			WebElement username = driver.findElement(By.xpath("//input[@id='userName']"));
 			username.sendKeys("mallikarjunamg21@outlook.com");
 			WebElement cont = driver.findElement(By.xpath("//button[@id='checkUser']"));
 			cont.click();
-			driver.switchTo().defaultContent();
+
 			Robot k= new Robot();
 			k.keyPress(KeyEvent.VK_CONTROL);
 			k.keyPress(KeyEvent.VK_T);
 			k.keyRelease(KeyEvent.VK_CONTROL);
 			k.keyRelease(KeyEvent.VK_T);
-			driver.navigate().to("https//www.yahoomail.com");
+			driver.navigate().to(frameUrl);
+			//driver.switchTo().defaultContent();
+
+//			Robot k= new Robot();
+//			k.keyPress(KeyEvent.VK_CONTROL);
+//			k.keyPress(KeyEvent.VK_T);
+//			k.keyRelease(KeyEvent.VK_CONTROL);
+//			k.keyRelease(KeyEvent.VK_T);
+			String window = driver.getWindowHandle();
+			driver.get("https://mail.yahoo.com/");
 
 
 
 		}
 
-		@AfterMethod
+//		@AfterTest
+//		public void screenshot() throws IOException {
+//			Screenshot screenshot;
+//			screenshot = new AShot().takeScreenshot(driver);
+//			//screenshot = new AShot().shootingStrategy(ShootingStrategies.viewportPasting(1000)).takeScreenshot(driver);
+//			ImageIO.write(screenshot.getImage(), "jpg", new File("C:/Users/Lenovo/IdeaProjects/Snapdeal_Automation/Screenshots/" + Test.class + ".jpg"));
+//		}
+
+	@Test
+	public void navigations() throws InterruptedException {
+		driver.navigate().to("https://www.google.com/");
+		//driver.navigate().back();
+		driver.navigate().forward();
+		Thread.sleep(3000);
+		driver.navigate().refresh();
+		Thread.sleep(3000);
+
+	}
+	@AfterMethod
 		public void tearDown() {
-			Screenshot screenshot = new AShot().shootingStrategy(ShootingStrategies.viewportPasting(1000)).takeScreenshot(driver);
-			ImageIO.write(screenshot.getImage(), "jpg", new File("C:/Users/Lenovo/IdeaProjects/Snapdeal_Automation/Screenshots/"+Test.class+".jpg"));
-					
 			driver.close();
 		}
 
